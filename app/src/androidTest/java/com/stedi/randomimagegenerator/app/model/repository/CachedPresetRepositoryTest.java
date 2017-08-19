24497dca6979e581f3a9dbf39bff75359bbb7def
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2017-08-19 12:04:06 +0200

    using FakePresetRepository for CachedPresetRepositoryTest

--- app/src/androidTest/java/com/stedi/randomimagegenerator/app/model/repository/CachedPresetRepositoryTest.java
+++ app/src/androidTest/java/com/stedi/randomimagegenerator/app/model/repository/CachedPresetRepositoryTest.java
@@ -1,15 +1,11 @@
 package com.stedi.randomimagegenerator.app.model.repository;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.SparseArray;
 
 import com.stedi.randomimagegenerator.Quality;
 import com.stedi.randomimagegenerator.app.model.data.GeneratorType;
 import com.stedi.randomimagegenerator.app.model.data.Preset;
 import com.stedi.randomimagegenerator.app.model.data.generatorparams.base.GeneratorParams;
-import com.stedi.randomimagegenerator.app.other.Utils;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,41 +21,15 @@ public class CachedPresetRepositoryTest {
     private PresetRepository target;
     private CachedPresetRepository cachedPresetRepository;
 
-    public static class FakeTarget implements PresetRepository {
-        private final SparseArray<Preset> fake = new SparseArray<>();
-
-        @Override
-        public void save(@NonNull Preset preset) throws Exception {
-            fake.put(preset.getId(), preset);
-        }
-
-        @Override
-        public void remove(int id) throws Exception {
-            fake.remove(id);
-        }
-
-        @Nullable
-        @Override
-        public Preset get(int id) throws Exception {
-            return fake.get(id);
-        }
-
-        @NonNull
-        @Override
-        public List<Preset> getAll() throws Exception {
-            return Utils.sparseArrayToList(fake);
-        }
-    }
-
     @Before
     public void before() throws Exception {
-        target = spy(new FakeTarget());
+        target = spy(new FakePresetRepository(0));
         cachedPresetRepository = new CachedPresetRepository(target);
     }
 
     @Test
     public void test1() throws Exception {
-        Preset preset = createPresetWithId(10);
+        Preset preset = createPreset();
 
         cachedPresetRepository.remove(20);
         verify(target, times(1)).remove(20);
@@ -68,14 +38,14 @@ public class CachedPresetRepositoryTest {
         cachedPresetRepository.save(preset);
         verify(target, times(1)).save(preset);
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().get(10) == preset);
+        assertTrue(cachedPresetRepository.getCache().get(1) == preset);
 
-        cachedPresetRepository.remove(10);
-        verify(target, times(1)).remove(10);
+        cachedPresetRepository.remove(1);
+        verify(target, times(1)).remove(1);
         assertTrue(cachedPresetRepository.getCache().size() == 0);
 
-        assertNull(cachedPresetRepository.get(10));
-        verify(target, times(1)).get(10);
+        assertNull(cachedPresetRepository.get(1));
+        verify(target, times(1)).get(1);
         assertTrue(cachedPresetRepository.getCache().size() == 0);
 
         assertTrue(cachedPresetRepository.getAll().isEmpty());
@@ -88,7 +58,7 @@ public class CachedPresetRepositoryTest {
 
     @Test
     public void test2() throws Exception {
-        Preset preset = createPresetWithId(10);
+        Preset preset = createPreset();
 
         assertTrue(cachedPresetRepository.get(20) == null);
         verify(target, times(1)).get(20);
@@ -97,33 +67,33 @@ public class CachedPresetRepositoryTest {
         cachedPresetRepository.save(preset);
         verify(target, times(1)).save(preset);
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().get(10) == preset);
+        assertTrue(cachedPresetRepository.getCache().get(1) == preset);
 
-        assertTrue(cachedPresetRepository.get(10) == preset);
+        assertTrue(cachedPresetRepository.get(1) == preset);
         verifyZeroInteractions(target);
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().get(10) == preset);
+        assertTrue(cachedPresetRepository.getCache().get(1) == preset);
 
         assertTrue(cachedPresetRepository.getAll().size() == 1);
         verify(target, times(1)).getAll();
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().get(10) == preset);
+        assertTrue(cachedPresetRepository.getCache().get(1) == preset);
 
         assertTrue(cachedPresetRepository.getAll().size() == 1);
         verifyZeroInteractions(target);
 
-        cachedPresetRepository.remove(10);
-        verify(target, times(1)).remove(10);
+        cachedPresetRepository.remove(1);
+        verify(target, times(1)).remove(1);
         assertTrue(cachedPresetRepository.getCache().size() == 0);
 
-        assertNull(cachedPresetRepository.get(10));
-        verify(target, times(1)).get(10);
+        assertNull(cachedPresetRepository.get(1));
+        verify(target, times(1)).get(1);
         assertTrue(cachedPresetRepository.getCache().size() == 0);
     }
 
     @Test
     public void test3() throws Exception {
-        Preset preset = createPresetWithId(10);
+        Preset preset = createPreset();
 
         assertTrue(cachedPresetRepository.getAll().size() == 0);
         verify(target, times(1)).getAll();
@@ -132,57 +102,51 @@ public class CachedPresetRepositoryTest {
         cachedPresetRepository.save(preset);
         verify(target, times(1)).save(preset);
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().get(10) == preset);
+        assertTrue(cachedPresetRepository.getCache().get(1) == preset);
 
         assertTrue(cachedPresetRepository.getAll().size() == 1);
         verify(target, times(1)).getAll();
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().get(10) == preset);
+        assertTrue(cachedPresetRepository.getCache().get(1) == preset);
 
         assertTrue(cachedPresetRepository.getAll().size() == 1);
         verifyZeroInteractions(target);
 
-        assertTrue(cachedPresetRepository.get(10) == preset);
+        assertTrue(cachedPresetRepository.get(1) == preset);
         verifyZeroInteractions(target);
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().get(10) == preset);
+        assertTrue(cachedPresetRepository.getCache().get(1) == preset);
 
-        cachedPresetRepository.remove(10);
-        verify(target, times(1)).remove(10);
+        cachedPresetRepository.remove(1);
+        verify(target, times(1)).remove(1);
         assertTrue(cachedPresetRepository.getCache().size() == 0);
 
-        assertNull(cachedPresetRepository.get(10));
-        verify(target, times(1)).get(10);
+        assertNull(cachedPresetRepository.get(1));
+        verify(target, times(1)).get(1);
         assertTrue(cachedPresetRepository.getCache().size() == 0);
     }
 
     @Test
     public void test4() throws Exception {
-        FakeTarget fakeTarget = new FakeTarget();
-        for (int i = 0; i < 10; i++) {
-            Preset preset = createPresetWithId(i + 1);
-            fakeTarget.save(preset);
-        }
-
-        target = spy(fakeTarget);
+        target = spy(new FakePresetRepository(10));
         cachedPresetRepository = new CachedPresetRepository(target);
 
-        Preset preset = createPresetWithId(20);
+        Preset preset = createPreset();
 
         cachedPresetRepository.save(preset);
         verify(target, times(1)).save(preset);
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().indexOfKey(20) >= 0);
+        assertTrue(cachedPresetRepository.getCache().indexOfKey(11) >= 0);
 
         cachedPresetRepository.remove(1);
         verify(target, times(1)).remove(1);
         assertTrue(cachedPresetRepository.getCache().size() == 1);
-        assertTrue(cachedPresetRepository.getCache().indexOfKey(20) >= 0);
+        assertTrue(cachedPresetRepository.getCache().indexOfKey(11) >= 0);
 
         assertNotNull(cachedPresetRepository.get(2));
         verify(target, times(1)).get(2);
         assertTrue(cachedPresetRepository.getCache().size() == 2);
-        assertTrue(cachedPresetRepository.getCache().indexOfKey(20) >= 0);
+        assertTrue(cachedPresetRepository.getCache().indexOfKey(11) >= 0);
         assertTrue(cachedPresetRepository.getCache().indexOfKey(2) >= 0);
 
         List<Preset> allPresets = cachedPresetRepository.getAll();
@@ -202,10 +166,10 @@ public class CachedPresetRepositoryTest {
         assertNotNull(cachedPresetRepository.get(8));
         assertNotNull(cachedPresetRepository.get(9));
         assertNotNull(cachedPresetRepository.get(10));
-        assertNotNull(cachedPresetRepository.get(20));
+        assertNotNull(cachedPresetRepository.get(11));
         verifyZeroInteractions(target);
 
-        preset = createPresetWithId(30);
+        preset = createPreset();
         cachedPresetRepository.save(preset);
         verify(target, times(1)).save(preset);
 
@@ -219,11 +183,11 @@ public class CachedPresetRepositoryTest {
         verifyZeroInteractions(target);
     }
 
-    private Preset createPresetWithId(int id) {
+    private Preset createPreset() {
         Preset preset = new Preset("name",
                 GeneratorParams.createDefaultParams(GeneratorType.COLORED_NOISE),
                 Quality.png(), "path");
-        preset.setId(id);
+        preset.setTimestamp(System.currentTimeMillis());
         return preset;
     }
 }
