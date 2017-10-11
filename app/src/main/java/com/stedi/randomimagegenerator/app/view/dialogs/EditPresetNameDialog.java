e7cd74bd79e0dab8f5ea38f10cd40287b8379e5f
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2017-10-11 22:12:43 +0200

    EditPresetNameDialog material for all supported platforms

--- app/src/main/java/com/stedi/randomimagegenerator/app/view/dialogs/EditPresetNameDialog.java
+++ app/src/main/java/com/stedi/randomimagegenerator/app/view/dialogs/EditPresetNameDialog.java
@@ -1,11 +1,11 @@
 package com.stedi.randomimagegenerator.app.view.dialogs;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
