88c753458d78716e5f0d19b70e9713c299ec101f
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2017-10-21 13:37:36 +0200

    typing tests in ChooseSizeAndCountTest

---
+++ app/src/androidTest/java/com/stedi/randomimagegenerator/app/view/ChooseSizeAndCountTest.java
@@ -0,0 +1,127 @@
+package com.stedi.randomimagegenerator.app.view;
+
+import android.support.annotation.IdRes;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.stedi.randomimagegenerator.app.R;
+import com.stedi.randomimagegenerator.app.other.Utils;
+import com.stedi.randomimagegenerator.app.view.activity.HomeActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.stedi.randomimagegenerator.app.view.EspressoUtils.navigateInGenerationSteps;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.isEmptyString;
+
+@android.support.test.filters.LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ChooseSizeAndCountTest {
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
+
+    private final Integer[] WIDTH_HEIGHT_COUNT_IDS = new Integer[]{R.id.choose_size_and_count_et_width, R.id.choose_size_and_count_et_height, R.id.choose_size_and_count_et_count};
+    private final Integer[] WIDTH_RANGE_IDS = new Integer[]{R.id.choose_size_and_count_et_width_range_from, R.id.choose_size_and_count_et_width_range_to, R.id.choose_size_and_count_et_width_range_step};
+    private final Integer[] HEIGHT_RANGE_IDS = new Integer[]{R.id.choose_size_and_count_et_height_range_from, R.id.choose_size_and_count_et_height_range_to, R.id.choose_size_and_count_et_height_range_step};
+
+    @Test
+    public void typeTest() {
+        onView(withId(R.id.home_activity_empty_view))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.home_activity_fab), isDisplayed()))
+                .perform(click());
+
+        navigateInGenerationSteps("Generator", "Size/count");
+
+        type(WIDTH_HEIGHT_COUNT_IDS[0], "100");
+        type(WIDTH_HEIGHT_COUNT_IDS[1], "200");
+        type(WIDTH_HEIGHT_COUNT_IDS[2], "300");
+        verifyEmpty(WIDTH_RANGE_IDS);
+        verifyEmpty(HEIGHT_RANGE_IDS);
+        Utils.sleep(500);
+
+        type(WIDTH_RANGE_IDS[0], "100");
+        verifyText(WIDTH_RANGE_IDS[1], "1");
+        verifyText(WIDTH_RANGE_IDS[2], "1");
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[0]);
+        verifyText(WIDTH_HEIGHT_COUNT_IDS[1], "200");
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[2]);
+        Utils.sleep(500);
+
+        type(HEIGHT_RANGE_IDS[0], "100");
+        verifyText(HEIGHT_RANGE_IDS[1], "1");
+        verifyText(HEIGHT_RANGE_IDS[2], "1");
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[0]);
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[1]);
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[2]);
+        Utils.sleep(500);
+
+        type(WIDTH_HEIGHT_COUNT_IDS[2], "300");
+        verifyText(WIDTH_HEIGHT_COUNT_IDS[0], "1");
+        verifyText(WIDTH_HEIGHT_COUNT_IDS[1], "1");
+        verifyEmpty(WIDTH_RANGE_IDS);
+        verifyEmpty(HEIGHT_RANGE_IDS);
+        Utils.sleep(500);
+
+        type(WIDTH_RANGE_IDS[2], "100");
+        verifyText(WIDTH_RANGE_IDS[0], "1");
+        verifyText(WIDTH_RANGE_IDS[1], "1");
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[0]);
+        verifyText(WIDTH_HEIGHT_COUNT_IDS[1], "1");
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[2]);
+        Utils.sleep(500);
+
+        type(HEIGHT_RANGE_IDS[2], "100");
+        verifyText(HEIGHT_RANGE_IDS[0], "1");
+        verifyText(HEIGHT_RANGE_IDS[1], "1");
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[0]);
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[1]);
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[2]);
+        Utils.sleep(500);
+
+        type(WIDTH_HEIGHT_COUNT_IDS[1], "100");
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[0]);
+        verifyEmpty(WIDTH_HEIGHT_COUNT_IDS[2]);
+        verifyText(WIDTH_RANGE_IDS[2], "100");
+        verifyText(WIDTH_RANGE_IDS[0], "1");
+        verifyText(WIDTH_RANGE_IDS[1], "1");
+        verifyEmpty(HEIGHT_RANGE_IDS);
+        Utils.sleep(500);
+
+        type(WIDTH_HEIGHT_COUNT_IDS[0], "100");
+        verifyText(WIDTH_HEIGHT_COUNT_IDS[1], "100");
+        verifyText(WIDTH_HEIGHT_COUNT_IDS[2], "1");
+        verifyEmpty(WIDTH_RANGE_IDS);
+        verifyEmpty(HEIGHT_RANGE_IDS);
+        Utils.sleep(500);
+    }
+
+    private void type(@IdRes int id, String text) {
+        onView(allOf(withId(id), isDisplayed()))
+                .perform(scrollTo(), click(), clearText(), typeText(text));
+    }
+
+    private void verifyEmpty(@IdRes Integer... ids) {
+        for (int id : ids) {
+            onView(allOf(withId(id), isDisplayed()))
+                    .check(matches(withText(isEmptyString())));
+        }
+    }
+
+    private void verifyText(@IdRes int id, String text) {
+        onView(allOf(withId(id), isDisplayed()))
+                .check(matches(withText(text)));
+    }
+}
