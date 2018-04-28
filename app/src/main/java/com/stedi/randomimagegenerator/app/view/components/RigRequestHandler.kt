3d119346d96e3f16bf23e1d50a62b175dd9cbf25
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2018-04-28 18:46:47 +0200

    fixed incorrect generation in RigRequestHandler

--- app/src/main/java/com/stedi/randomimagegenerator/app/view/components/RigRequestHandler.kt
+++ app/src/main/java/com/stedi/randomimagegenerator/app/view/components/RigRequestHandler.kt
@@ -7,6 +7,7 @@ import com.squareup.picasso.Picasso
 import com.squareup.picasso.Request
 import com.squareup.picasso.RequestHandler
 import com.stedi.randomimagegenerator.ImageParams
+import com.stedi.randomimagegenerator.Quality
 import com.stedi.randomimagegenerator.Rig
 import com.stedi.randomimagegenerator.app.model.data.GeneratorType
 import com.stedi.randomimagegenerator.app.model.data.generatorparams.base.GeneratorParams
@@ -32,23 +33,19 @@ class RigRequestHandler : RequestHandler() {
     }
 
     override fun load(request: Request, networkPolicy: Int): Result {
-        val mainType = getMainType(request.uri)
-        val secondType = getSecondType(request.uri)
+        val generatorParams = createGeneratorParams(getMainType(request.uri), getSecondType(request.uri))
         val size = getSize(request.uri)
+        val format = getCompressFormat(request.uri)
+        val quality = getQualityValue(request.uri)
 
-        val generatorParams = createGeneratorParams(mainType, secondType)
-        val bitmap = generate(generatorParams, size[0], size[1])
+        val bitmap = generate(generatorParams, size[0], size[1], Quality(format, quality))
         if (bitmap == null) {
             throw IOException("failed to generate bitmap")
         }
 
-        val format = getCompressFormat(request.uri)
-
         if (format == CompressFormat.PNG) {
             return RequestHandler.Result(bitmap, Picasso.LoadedFrom.MEMORY)
         } else {
-            val quality = getQualityValue(request.uri)
-
             val source = Okio.source(toInputStream(bitmap, format, quality))
             return RequestHandler.Result(source, Picasso.LoadedFrom.MEMORY)
         }
@@ -79,9 +76,19 @@ class RigRequestHandler : RequestHandler() {
         }
     }
 
-    private fun generate(generatorParams: GeneratorParams, width: Int, height: Int): Bitmap? {
+    private fun getCompressFormat(uri: Uri): CompressFormat {
+        val format = uri.getQueryParameter("format")
+        return CompressFormat.values().first { it.name == format }
+    }
+
+    private fun getQualityValue(uri: Uri): Int {
+        return uri.getQueryParameter("quality").toInt()
+    }
+
+    private fun generate(generatorParams: GeneratorParams, width: Int, height: Int, quality: Quality): Bitmap? {
         var result: Bitmap? = null
         Rig.Builder().setGenerator(generatorParams.getGenerator()).setCount(1).setFixedSize(width, height)
+                .setQuality(quality)
                 .setCallback(object : GenerateCallback {
                     override fun onGenerated(imageParams: ImageParams, bitmap: Bitmap) {
                         result = bitmap
@@ -90,15 +97,6 @@ class RigRequestHandler : RequestHandler() {
                     override fun onFailedToGenerate(imageParams: ImageParams, e: Exception) {}
                 }).build().generate()
         return result
-    }
-
-    private fun getCompressFormat(uri: Uri): CompressFormat {
-        val format = uri.getQueryParameter("format")
-        return CompressFormat.values().first { it.name == format }
-    }
-
-    private fun getQualityValue(uri: Uri): Int {
-        return uri.getQueryParameter("quality").toInt()
     }
 
     private fun toInputStream(bitmap: Bitmap, format: Bitmap.CompressFormat, quality: Int): InputStream {
