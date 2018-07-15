878b5cac2f87175a3c5b2741af386177cf624f90
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2018-07-15 12:09:15 +0200

    performance improvements

--- app/src/main/java/com/stedi/randomimagegenerator/app/view/components/HSVRangeBar.kt
+++ app/src/main/java/com/stedi/randomimagegenerator/app/view/components/HSVRangeBar.kt
@@ -1,10 +1,7 @@
 package com.stedi.randomimagegenerator.app.view.components
 
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import com.edmodo.rangebar.RangeBar
 import com.stedi.randomimagegenerator.app.R
@@ -13,7 +10,11 @@ class HSVRangeBar : RangeBar {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val HSV = FloatArray(3)
 
-    private val rangeBarLRPadding by lazy {
+    private var barBitmap: Bitmap? = null
+    private var barWidth = 0
+    private var barHeight = 0
+
+    private val barLRPadding by lazy {
         // RangeBar uses this drawable to set left and right paddings
         val bitmap = BitmapFactory.decodeResource(resources, com.edmodo.rangebar.R.drawable.seek_thumb_normal)
         bitmap.width / 2f
@@ -33,23 +34,39 @@ class HSVRangeBar : RangeBar {
         setThumbColorPressed(resources.getColor(R.color.colorAccent))
     }
 
-    override fun onDraw(canvas: Canvas) {
-        val barWidth = width - rangeBarLRPadding * 2f
-        val barHeight = height.toFloat()
-        val hueStep = Math.max(barWidth / 361f, 1f)
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        if (barWidth != w || barHeight != h) {
+            barWidth = w
+            barHeight = h
+            post { invalidateBarBitmap() }
+        }
+    }
 
+    override fun onDraw(canvas: Canvas) {
+        val barBitmap = barBitmap
+        if (barBitmap != null) {
+            canvas.drawBitmap(barBitmap, barLRPadding, 0f, null)
+        }
+        super.onDraw(canvas)
+    }
+
+    private fun invalidateBarBitmap() {
+        val barWidth = barWidth - barLRPadding * 2f
+        val barHeight = barHeight.toFloat()
+
+        barBitmap = Bitmap.createBitmap(barWidth.toInt(), barHeight.toInt(), Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(barBitmap)
+
+        val hueStep = Math.max(barWidth / 361f, 1f)
         var left = 0f
         for (hue in 0..360) {
             HSV[0] = hue.toFloat()
             paint.color = Color.HSVToColor(HSV)
-
-            val xStart = left + rangeBarLRPadding
-            val xEnd = xStart + hueStep
-            canvas.drawRect(xStart, 0f, xEnd, barHeight, paint)
-
+            canvas.drawRect(left, 0f, left + hueStep, barHeight, paint)
             left += hueStep
         }
 
-        super.onDraw(canvas)
+        invalidate()
     }
 }
