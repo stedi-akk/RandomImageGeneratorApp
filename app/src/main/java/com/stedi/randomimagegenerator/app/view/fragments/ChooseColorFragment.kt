e13a540f1b1d6fe7d13610dbdfa366d140110d8d
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2018-07-17 20:45:10 +0200

    early ChooseColorFragment layout

--- app/src/main/java/com/stedi/randomimagegenerator/app/view/fragments/ChooseColorFragment.kt
+++ app/src/main/java/com/stedi/randomimagegenerator/app/view/fragments/ChooseColorFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import butterknife.BindView
 import com.stedi.randomimagegenerator.app.R
+import com.stedi.randomimagegenerator.app.view.components.HSVRangeBar
 import com.stedi.randomimagegenerator.app.view.fragments.base.GenerationFragment
 
 class ChooseColorFragment : GenerationFragment() {
+
+    @BindView(R.id.choose_color_fragment_range_bar) lateinit var rangeBar: HSVRangeBar
+    @BindView(R.id.choose_color_fragment_cb_use_light) lateinit var cbUseLight: CheckBox
+    @BindView(R.id.choose_color_fragment_cb_use_dark) lateinit var cbUseDark: CheckBox
+    @BindView(R.id.choose_color_fragment_cb_grayscale) lateinit var cbGrayscale: CheckBox
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.choose_color_fragment, container, false)
